d270bc7ebd86417d9a176fdd2976b91e323b9801
Juan Ojeda <juan@juanojeda.com>
2017-12-01 00:45:42 +1100

    add a test to ensure that fetching data creates the right actions

--- src/js/actions/PropertiesActions.js
+++ src/js/actions/PropertiesActions.js
@@ -1,14 +1,37 @@
+import 'whatwg-fetch';
+
 export const REQUEST_FETCH_PROPERTIES = 'REQUEST_FETCH_PROPERTIES';
 export const RECEIVE_PROPERTIES       = 'RECEIVE_PROPERTIES';
 export const ADD_PROPERTY             = 'ADD_PROPERTY';
 export const REMOVE_PROPERTY          = 'REMOVE_PROPERTY';
+export const PROPERTIES_API = '/data/properties.json';
+
+export const requestFetchProperties = () => ({
+  type: REQUEST_FETCH_PROPERTIES
+})
+
 
 // action for requesting fetch
+export const fetchProperties = () => dispatch => {
+  dispatch(requestFetchProperties());
+
+  const propertiesPromise = fetch(PROPERTIES_API)
+    .then(response => response.json())
+    .then(json => {
+      dispatch(receiveFetchedProperties(json));
+    })
+    .catch(error => {
+      throw(error)
+    });
+
+  return propertiesPromise;
+
+}
 
 // action for receiving fetched properties
 export const receiveFetchedProperties = propertiesData => ({
   propertiesData,
-  action: RECEIVE_PROPERTIES
+  type: RECEIVE_PROPERTIES
 });
 
 // action for adding properties to saved property store
--- src/js/actions/PropertiesActions.test.js
+++ src/js/actions/PropertiesActions.test.js
@@ -1,23 +1,51 @@
-import * as PropertiesActions from './PropertiesActions';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
-const mockPropertyData = {
-  results: [
-    'test'
-  ],
-  saved: [
-    'test'
-  ]
-};
+import fetchMock from 'fetch-mock';
+
+import * as PropertiesActions from './PropertiesActions';
+import mockData from '../../data/properties.json';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('PropertiesActions', () => {
+
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
   it('should create an action to receive fetched properties data', () => {
-    const propertiesData = mockPropertyData;
+    const propertiesData = mockData;
     const expectedAction = {
       propertiesData,
-      action: PropertiesActions.RECEIVE_PROPERTIES
+      type: PropertiesActions.RECEIVE_PROPERTIES
     };
 
     expect(PropertiesActions.receiveFetchedProperties(propertiesData)).toEqual(expectedAction);
+  });
+
+  it('should creates RECEIVE_PROPERTIES once the properties have been fetched', () => {
+
+    fetchMock.getOnce(PropertiesActions.PROPERTIES_API, mockData);
+
+    const propertiesData = mockData;
+    const expectedActions = [
+      {type: PropertiesActions.REQUEST_FETCH_PROPERTIES},
+      {
+      propertiesData,
+      type: PropertiesActions.RECEIVE_PROPERTIES
+    }];
+    const store = mockStore({
+      results: [],
+      saved: []
+    })
+
+    return store.dispatch(PropertiesActions.fetchProperties()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+
   });
 
 });
