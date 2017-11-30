7864ac80e1efb2bf894a2949f03ef0f47abd96ec
Juan Ojeda <juan@juanojeda.com>
2017-12-01 02:09:59 +1100

    create reducer handlers and tests for PropertiesReducer, to handle: - receive properties, - add properties to save list, - remove properties from save list

--- src/js/reducers/PropertiesReducer.js
+++ src/js/reducers/PropertiesReducer.js
@@ -1,4 +1,8 @@
 import assign from 'lodash/assign';
+import uniqBy from 'lodash/uniqBy';
+import filter from 'lodash/filter';
+
+import * as Actions from '../actions/PropertiesActions';
 
 const initialState = {
   results: [],
@@ -7,7 +11,28 @@ const initialState = {
 
 const PropertiesReducer = (state = initialState, action) => {
   switch (action.type) {
+    case Actions.RECEIVE_PROPERTIES: {
+      const newState = action.propertiesData;
 
+      return newState;
+    }
+    case Actions.ADD_PROPERTY: {
+      const newSavedProperties = uniqBy([...state.saved, action.property], 'id');
+      const newState = assign({}, state, {
+        saved: newSavedProperties
+      });
+
+      return newState;
+    }
+    case Actions.REMOVE_PROPERTY: {
+      const newSavedProperties = filter(state.saved, property => property.id !== action.propertyId);
+      const newState = assign({}, state, {
+        saved: newSavedProperties
+      });
+
+      return newState;
+
+    }
     default: {
       return state;
     }
---
+++ src/js/reducers/PropertiesReducer.test.js
@@ -0,0 +1,99 @@
+import PropertiesReducer from './PropertiesReducer';
+import * as ActionsTypes from '../actions/PropertiesActions';
+
+import mockReceivedData from '../../data/properties.json';
+
+/* eslint-disable quotes */
+const mockProperty = {
+  "price": "$726,500",
+  "agency": {
+    "brandingColors": {
+      "primary": "#ffe512"
+    },
+    "logo": "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
+  },
+  "id": "1",
+  "mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
+};/* eslint-enable quotes */
+
+describe('Properties Reducer', () => {
+
+  it('should return the initial state', () => {
+    expect(PropertiesReducer(undefined, {})).toEqual({
+      results: [],
+      saved: []
+    });
+  });
+
+  it('should handle receiving fetched properties', () => {
+
+    // on a fresh reducer
+    expect(PropertiesReducer({}, {
+      type: ActionsTypes.RECEIVE_PROPERTIES,
+      propertiesData: mockReceivedData
+    })).toEqual(mockReceivedData);
+
+    // on a populated reducer
+    expect(PropertiesReducer(
+      {
+        results: ['test 1', 'test 2'],
+        saved: ['test1', 'test2']
+      },
+      {
+        type: ActionsTypes.RECEIVE_PROPERTIES,
+        propertiesData: mockReceivedData
+      }
+    )).toEqual(mockReceivedData);
+  });
+
+  it('should handle adding properties', () => {
+
+    // can add to an empty array of saved properties
+    expect(PropertiesReducer(
+      {
+        results: [],
+        saved: []
+      },
+      {
+        type: ActionsTypes.ADD_PROPERTY,
+        property: mockProperty
+      })
+    ).toEqual({
+      results: [],
+      saved: [mockProperty]
+    });
+
+    // can handle duplicates by not adding them
+    expect(PropertiesReducer(
+      {
+        results: [],
+        saved: [mockProperty]
+      },
+      {
+        type: ActionsTypes.ADD_PROPERTY,
+        property: mockProperty
+      })
+    ).toEqual({
+      results: [],
+      saved: [mockProperty]
+    });
+  });
+
+  it('should handle removing properties', () => {
+
+    // can add to an empty array of saved properties
+    expect(PropertiesReducer(
+      {
+        results: [],
+        saved: [mockProperty]
+      },
+      {
+        type: ActionsTypes.REMOVE_PROPERTY,
+        propertyId: '1'
+      })
+    ).toEqual({
+      results: [],
+      saved: []
+    });
+  });
+});
