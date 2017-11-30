5497f2fa29fa09ce176a6178e9b85659e7712dfe
Juan Ojeda <juan@juanojeda.com>
2017-11-30 19:59:39 +1100

    add fonts

--- gulp/extras.js
+++ gulp/extras.js
@@ -5,6 +5,7 @@ module.exports = (gulp) => {
   return () => {
     return gulp.src([
       'src/data/**/*',
+      'src/fonts/**/*',
       `${config.paths.src}/*.{ico,txt,png,config,html}`
     ], {'base': `${config.paths.src}`})
     .pipe(gulp.dest(`${config.paths.dist}/`));
