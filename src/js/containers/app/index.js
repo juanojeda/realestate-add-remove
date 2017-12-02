efff665c61358a6d0c39bb84de5e210d6fae16e2
Juan Ojeda <juan@juanojeda.com>
2017-12-02 11:11:10 +1100

    add animated loader for App

--- src/js/containers/app/index.js
+++ src/js/containers/app/index.js
@@ -1,31 +1,74 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import assign from 'lodash/assign';
+
+import * as PropertiesActions from '../../actions/PropertiesActions';
 
 import Header from '../../components/header';
 import Property from '../../components/property';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  componentWillMount() {
+
+  }
+
   render() {
+
+    const { isLoading } = this.state;
+
     return (
       <div>
         <Header />
-        <div className="property-selector">
-          <div className="property-selector__list">
-            <div className="property-selector__list-header">
-              <h3>Results</h3>
+        {
+          isLoading ?
+          <div className="loader">
+            <div className="loader__spinner"></div>
+          </div>
+          : <div className="property-selector">
+            <div className="property-selector__list">
+              <div className="property-selector__list-header">
+                <h3>Results</h3>
+              </div>
+              <Property />
+              <Property />
+              <Property />
             </div>
-            <Property />
-            <Property />
-            <Property />
-          </div>
 
-          <div className="property-selector__list">
-            <h3>Saved Properties</h3>
-            <Property isSaved={true} />
+            <div className="property-selector__list">
+              <h3>Saved Properties</h3>
+              <Property isSaved={true} />
+            </div>
           </div>
-        </div>
+        }
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { results: allProperties, saved } = state.PropertiesReducer;
+
+  return {
+    allProperties,
+    saved
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(assign({}, PropertiesActions), dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
